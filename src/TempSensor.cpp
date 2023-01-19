d0d25e573add4536dd7edf0fe88c3f6783fcdc4a
Edward Hails <onfe@users.noreply.github.com>
2023-01-19 22:58:04 +0000

    Second attempt at temperature trend computation.

--- src/TempSensor.cpp
+++ src/TempSensor.cpp
@@ -1,15 +1,22 @@
 #include "TempSensor.hpp"
 
 void TempSensor::update() {
-    // There's not much point reading a value from the ADC more frequently than 20 times a second as the heater temps
+    // There's not much point reading a value from the ADC more frequently than 40 times a second as the heater temps
     // don't fluctuate quickly, and reading less frequently makes it easier to average over time.
-    if (millis() - lastUpdate < 50) return;
+    if (millis() - lastUpdate < 25) return;
     lastUpdate = millis();
 
     float temp = calculate();
 
     if (temp > 500 || temp < -15) {
         Serial.println(temp);
+        discarded++;
+        if (discarded <= 5) {
+
+            return;
+        }
+    } else {
+        discarded = 0;
     }
 
     // store the temperature as an integer multiplied by 10, to give up 1dp of resolution.
@@ -17,22 +24,21 @@ void TempSensor::update() {
     this->temps[tempIndex] = round(temp10);
 
     ++tempIndex;
-    if (tempIndex >= 32) {
+    if (tempIndex >= 64) {
         tempIndex = 0;
         tempFilled = true;
     }
 
     // Every time we've looped all the way through, add a temp to the history.
-    if (tempIndex == 0) {
+    if (tempIndex % 16 == 0) {
         temp10 = this->temp() * 10;
 
-        this->history[histIndex] = round(temp10);
-
-        ++histIndex;
-        if (histIndex >= 16) {
-            histIndex = 0;
-            histFilled = true;
+        // shuffle the folks along.
+        for (int i = 1; i < 64; i++) {
+            this->history[i] = this->history[i - 1];
         }
+
+        this->history[0] = round(temp10);
     }
 }
 
@@ -55,14 +61,14 @@ float TempSensor::steinhart(double resistance) const {
 }
 
 float TempSensor::temp() {
-    int size = tempFilled ? 32 : tempIndex;
+    int size = tempFilled ? 64 : tempIndex;
 
     float t10 = average(temps, size);
     return t10 / 10.0f;
 }
 
 int TempSensor::average(const int *arr, int size) {
-    int total = 0;
+    long total = 0;
     if (size < 1) return 0;
 
     for (int i = 0; i < size; ++i) {
@@ -73,27 +79,43 @@ int TempSensor::average(const int *arr, int size) {
 }
 
 bool TempSensor::increasing() {
-    int size = histFilled ? 16 : histIndex;
-    if (size < 2) {
-        return false;
+    // int size = histFilled ? 16 : histIndex;
+    // if (size < 2) {
+    //     return false;
+    // }
+
+    // int size = 16;
+
+    // int newer = average(history, 4);
+    // int older = average(history + 4, 12);
+
+    // return older + (delta * 10) < newer;
+    return rateOfChange() > 0.5;
+}
+
+float TempSensor::rateOfChange() {
+    float total = 0;
+    const unsigned char size = 64;
+    for (unsigned char i = 0; i < size - 1; i++) {
+        total += (this->history[i] - this->history[i + 1]);
     }
 
-    int older = average(history, size / 2);
-    int newer = average(history + (size / 2), size - (size / 2));
-
-    return older + (delta * 10) < newer;
+    return total / (float)(size - 1) * 2.5f;
 }
 
 bool TempSensor::decreasing() {
-    int size = histFilled ? 16 : histIndex;
-    if (size < 2) {
-        return false;
-    }
+    // int size = histFilled ? 16 : histIndex;
+    // if (size < 2) {
+    //     return false;
+    // }
 
-    int older = average(history, size / 2);
-    int newer = average(history + (size / 2), size - (size / 2));
+    // int size = 16;
 
-    return older - (delta * 10) > newer;
+    // int newer = average(history, 4); // first 4
+    // int older = average(history + 8, 8); // next 4
+
+    // return older - (delta * 10) > newer;
+    return rateOfChange() < -0.5;
 }
 
 bool TempSensor::stable() {
