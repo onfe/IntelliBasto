4c0ac1000c2153de22c654aea3324c8336b6d7ab
Edward Hails <onfe@users.noreply.github.com>
2023-02-05 21:56:23 +0000

    Add notes to future me for fuel tuning.

--- src/FuelMap.cpp
+++ src/FuelMap.cpp
@@ -1,11 +1,23 @@
 #include "FuelMap.h"
 #include <Arduino.h>
 
-unsigned char fuelmap(double hz) {
-    // 1.5hz -> 24
-    // 2.0hz -> 32
+// At 4.5hz, tweaking the power gives the following result:
+// 1.55 is very sooty
+// 1.6 is a bit sooty
+// 1.65 is a bit sooty, a few pops
+// 1.7 todo
+// 1.75 todo
+// 1.8 pops a lot then goes out.
 
-    // double fanspeed = 16.0 * hz;
+// I think we can run more lean at higher burn rates, as the combustion chamber
+// gets hotter, but I haven't tested this. The other reason we can struggle to
+// run lean is due to soot on the nebuliser screen and combustion chamber.
+
+// A solution could be to run hotter (~6-7hz) at startup then tone down to 4-4.5hz.
+// This should in theory get the chamber nice and hot for at least a few minutes
+// which might reduce soot buildup over time.
+
+unsigned char fuelmap(double hz) {
     double fanspeed = 8.0 * pow(hz, 1.6);
 
     unsigned char bounded = (unsigned char)constrain((long)round(fanspeed), (long)0, (long)255);
